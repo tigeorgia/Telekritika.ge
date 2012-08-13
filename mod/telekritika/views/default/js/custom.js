f66933f40a85354427e51d9f8f64960c73ba6e4d
George Lubaretsi <gio@transparency.ge>
2012-08-13 17:57:57 +0400

    Custom js file for text selector #6

---
+++ mod/telekritika/views/default/js/custom.js
@@ -0,0 +1,25 @@
+jQuery.fn.selText = function() {
+    var obj = this[0];
+    if ($.browser.msie) {
+        var range = obj.offsetParent.createTextRange();
+        range.moveToElementText(obj);
+        range.select();
+    } else if ($.browser.mozilla || $.browser.opera || $.browser.chrome) {
+        var selection = obj.ownerDocument.defaultView.getSelection();
+        var range = obj.ownerDocument.createRange();
+        range.selectNodeContents(obj);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    } else if ($.browser.safari) {
+        var selection = obj.ownerDocument.defaultView.getSelection();
+        selection.setBaseAndExtent(obj, 0, obj, 1);
+    }
+    return this;
+}
+
+$(function() {
+    $("img.selecttextb").click(function() {
+       //$(this).selText().addClass("selected");
+        $(this).parent().parent().prev().prev().selText().addClass("selectedsr");
+    });
+});
